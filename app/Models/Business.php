d04272398749e0be7c08eb81bdf0ae8fa8573bef
Samuel Gaona <azureussme@gmail.com>
2021-06-17 01:55:15 -0500

    Corrección en relaciones de Business

--- app/Models/Business.php
+++ app/Models/Business.php
@@ -2,31 +2,17 @@
 
 namespace App\Models;
 
+use Exception;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
-use Auth;
-use DB;
 
 
-
-class Business extends Model {
+class Business extends Model
+{
     use HasFactory;
 
-
-    /**
-     * Table in database.
-     *
-     * @var string
-     */
-    protected $table = "business";
-
-    /**
-     * Primary key in table.
-     *
-     * @var string
-     */
-    protected $primaryKey = "businessId";
 
     /**
      * True if there are columns for creation and update dates.
@@ -34,42 +20,27 @@ class Business extends Model {
      * @var boolean
      */
     public $timestamps = false;
+    /**
+     * Table in database.
+     *
+     * @var string
+     */
+    protected $table = "business";
+    /**
+     * Primary key in table.
+     *
+     * @var string
+     */
+    protected $primaryKey = "businessId";
 
-    public function getAllBusiness() {
-        return $this::with('phoneNumbers')->get();
-    }
-
-    public function getBusinessBySlug($businessSlug) {
-        return $this::with(['phoneNumbers', 'category'])->where('businessSlug', $businessSlug)->first();
-    }
-
-    public function saveBusiness() {
-        if (!$this->save()) {
-            throw new \Exception("An error occurred on saving business.");
-        }
-    }
-
-    public static function geBusinesstById($businessId) {
+    public static function geBusinesstById($businessId)
+    {
         return static::find($businessId);
-     }
-
-    public static function getSlugName($businessName){
-        $businessSlug = Str::of($businessName)->slug("-");
-
-        return $businessSlug;
     }
 
-    public function category() {
-        return $this->belongsTo(Category::class);
-    }
-
-    public function phoneNumbers() {
-        return $this->hasMany(PhoneNumber::class,"businessId", "phoneNumberId");
-    }
-
-    public static function createBusiness($userId, $categoryId, $businessName, $businessDescription, $phoneNumbers){
-
-        try{
+    public static function createBusiness($userId, $categoryId, $businessName, $businessDescription, $phoneNumbers)
+    {
+        try {
             DB::beginTransaction();
             $business = new Business();
             $business->userId = $userId;
@@ -78,33 +49,68 @@ class Business extends Model {
             $business->businessSlug = Business::getSlugName($businessName);
             $business->businessDescription = $businessDescription;
             $business->saveBusiness();
-            
 
-            foreach($phoneNumbers as $number) {
+
+            foreach ($phoneNumbers as $number) {
                 $phoneNumber = new PhoneNumber();
                 $phoneNumber->businessId = $business->businessId;
                 $phoneNumber->phoneNumber = $number["phoneNumber"];
                 $phoneNumber->numberType = $number["numberType"];
                 $phoneNumber->save();
             }
-           
+
             DB::commit();
-        }catch(\Exception $e){
+        } catch (Exception $e) {
             DB::rollBack();
             throw $e;
-        } 
+        }
     }
 
-    public function getReviews(){
+    public static function getSlugName($businessName)
+    {
+        $businessSlug = Str::of($businessName)->slug("-");
+
+        return $businessSlug;
+    }
+
+    public function saveBusiness()
+    {
+        if (!$this->save()) {
+            throw new Exception("An error occurred on saving business.");
+        }
+    }
+
+    public function getAllBusiness()
+    {
+        return $this::with('phoneNumbers')->get();
+    }
+
+    public function getBusinessBySlug($businessSlug)
+    {
+        return $this::with(['phoneNumbers', 'category'])->where('businessSlug', $businessSlug)->first();
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Category::class, 'categoryId');
+    }
+
+    public function phoneNumbers()
+    {
+        return $this->hasMany(PhoneNumber::class, "businessId");
+    }
+
+    public function getReviews()
+    {
         if (Business::where("businessId", $this->businessId)->count() == 0) {
             return response()->json([
                 'message' => 'The business doesn´t exit'
             ]);
-        }else{
-            $review = Review::orderBy("publish_date","DESC")->where("businessId", $this->businessId)->get();
+        } else {
+            $review = Review::orderBy("publish_date", "DESC")->where("businessId", $this->businessId)->get();
             return response()->json(
                 $review
-            ); 
-        }                 
+            );
+        }
     }
 }
